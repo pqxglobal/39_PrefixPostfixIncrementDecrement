dfe4628d203ccdebf4c8169a8d223c160a2a2144
pqx <l.r.slegt@gmail.com>
2022-04-13 12:36:59 +0200

    prefix/postfix added

--- main.cpp
+++ main.cpp
@@ -16,5 +16,22 @@ int main(){
 
     //=============================================================
 
+    std::cout << "============Postfix increment and decrement===============" << std::endl;
+    // reset value to 5
+    value = 5;
+
+    std::cout << "The value is (incrementing): " << value++ << std::endl;
+    std::cout << "The value is: " << value << std::endl;
+
+    std::cout << std::endl;
+
+    // decrement with postfix
+
+    // reset value to 5
+    value = 5;
+
+    std::cout << "The value is (decrementing): " << value-- << std::endl;
+    std::cout << "The value is: " << value << std::endl;
+
     return 0;
 }
